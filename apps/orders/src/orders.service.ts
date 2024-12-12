597af5e5cd5466a8685563d5677d9fe9a2c5ae6a
= <b.b.albayrak@hotmail.com>
2024-12-13 02:49:28 +0300

    Integrate RabbitMQ for order billing and enhance order creation with transaction support

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,13 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { OrderRepository } from './orders.repository';
 import { Order } from './schemas/order.schema';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepo: OrderRepository) {}
+  constructor(
+    private readonly ordersRepo: OrderRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
   async creteaOrder(body: any): Promise<Order> {
-    return await this.ordersRepo.create(body);
+    const session = await this.ordersRepo.startTransaction();
+    try {
+      const order = await this.ordersRepo.create(body, { session });
+      await lastValueFrom(this.billingClient.emit('created_order', { body }));
+      await session.commitTransaction();
+      return order;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    }
   }
 
   async getOrders(): Promise<Order[]> {
--- libs/common/src/rmq/rmq.module.ts
+++ libs/common/src/rmq/rmq.module.ts
@@ -1,7 +1,7 @@
 import { DynamicModule, Module } from '@nestjs/common';
-import { RmqService } from './rmq.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RmqService } from './rmq.service';
 
 interface RmqModuleOptions {
   name: string;
