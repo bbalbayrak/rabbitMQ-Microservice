332c925d5e43ced8bcc4071d78b83f66894444d7
= <b.b.albayrak@hotmail.com>
2024-12-13 00:56:49 +0300

    Refactor Docker setup and implement order creation with validation

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { orderDto } from './dto/create-order.dto';
+import { Request, Response } from 'express';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Post()
+  async createOrder(@Body() body: orderDto) {
+    return this.ordersService.creteaOrder(body);
+  }
+
   @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  async getOrders(@Res() res: Response) {
+    const orders = await this.ordersService.getOrders();
+    return res.status(HttpStatus.OK).json({
+      message: 'Orders fetched successfully',
+      data: orders,
+    });
   }
 }
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { OrderRepository } from './orders.repository';
+import { Order } from './schemas/order.schema';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly ordersRepo: OrderRepository) {}
+
+  async creteaOrder(body: any): Promise<Order> {
+    return await this.ordersRepo.create(body);
+  }
+
+  async getOrders(): Promise<Order[]> {
+    const orders = this.ordersRepo.find({});
+    return orders;
   }
 }
